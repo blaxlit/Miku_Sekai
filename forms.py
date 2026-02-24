b9e832a435b70835affe1b593756fbea7247007d
Thaweerat <thaweeratyordsathien@gmail.com>
2026-02-24 08:56:46 +0700

    feat: add form class for submitting new songs

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, URL, Optional
 
 class RegisterForm(FlaskForm):
     username = StringField('ชื่อผู้ใช้', validators=[DataRequired(), Length(min=4, max=50)])
@@ -11,3 +11,9 @@ class LoginForm(FlaskForm):
     username = StringField('ชื่อผู้ใช้', validators=[DataRequired()])
     password = PasswordField('รหัสผ่าน', validators=[DataRequired()])
     submit = SubmitField('เข้าสู่ระบบ')
+
+class AddSongForm(FlaskForm):
+    title = StringField('ชื่อเพลง', validators=[DataRequired(), Length(max=100)])
+    producer = StringField('โปรดิวเซอร์ (Vocaloid P)', validators=[DataRequired(), Length(max=100)])
+    youtube_url = StringField('ลิงก์ YouTube (ถ้ามี)', validators=[Optional(), URL()])
+    submit = SubmitField('เพิ่มเพลงลงคลัง')
