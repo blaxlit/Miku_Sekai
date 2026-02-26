0d3109474802e2fd73c0ceaa467fbeb8edb8ca6e
Thaweerat <thaweeratyordsathien@gmail.com>
2026-02-26 09:09:08 +0700

    fix models.py

--- models.py
+++ models.py
@@ -10,7 +10,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
     role = db.Column(db.String(20), nullable=False, default='user') # ⭐️ เพิ่มบรรทัดนี้: กำหนดค่าเริ่มต้นเป็น 'user' ธรรมดา
-    
+    songs = db.relationship('Song', backref='uploader', lazy=True)
     posts = db.relationship('Fanboard', backref='author', lazy=True)
 
 # (ส่วนคลาส Song และ Fanboard ปล่อยไว้เหมือนเดิมได้เลยครับ)
@@ -20,8 +20,11 @@ class Song(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     producer = db.Column(db.String(100), nullable=False)
-    youtube_url = db.Column(db.String(200), nullable=True)
+    youtube_url = db.Column(db.String(200))
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    # ⭐️ เพิ่มบรรทัดนี้: เก็บ ID ของคนที่เพิ่มเพลง
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 # 3. ตารางเว็บบอร์ด
 class Fanboard(db.Model):
